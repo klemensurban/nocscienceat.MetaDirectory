dc58649b19498ca9128264614364c14886e21c9f
Klemens Urban <klemens.urban@bmbwf.gv.at>
2026-01-17 00:25:34 +0100

    removed unused usings

--- Logging/MailLog.cs
+++ Logging/MailLog.cs
@@ -4,7 +4,6 @@ using Microsoft.Extensions.Configuration;
 using MimeKit;
 using nocscienceat.MetaDirectory.Logging.Models;
 using System;
-using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
